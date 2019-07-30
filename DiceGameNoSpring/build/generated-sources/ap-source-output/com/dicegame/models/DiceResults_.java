872f7b53d980194e11acb1fa2f462d06cb07fe61
xavieroldan <xavieroldan@gmail.com>
2019-07-30 11:52:51 +0200

    JPA done

---
+++ DiceGameNoSpring/build/generated-sources/ap-source-output/com/dicegame/models/DiceResults_.java
@@ -0,0 +1,17 @@
+package com.dicegame.models;
+
+import com.dicegame.models.DiceResultsPK;
+import com.dicegame.models.DiceRolls;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-07-30T11:52:26")
+@StaticMetamodel(DiceResults.class)
+public class DiceResults_ { 
+
+    public static volatile SingularAttribute<DiceResults, Integer> rollResult;
+    public static volatile SingularAttribute<DiceResults, DiceResultsPK> diceResultsPK;
+    public static volatile SingularAttribute<DiceResults, DiceRolls> diceRolls;
+
+}
