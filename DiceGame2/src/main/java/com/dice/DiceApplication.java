a990dbf2c53b283180870199407c048106be853a
xavieroldan <xavieroldan@gmail.com>
2019-09-12 18:24:27 +0200

    Working in error splash

--- DiceGame2/src/main/java/com/dice/DiceApplication.java
+++ DiceGame2/src/main/java/com/dice/DiceApplication.java
@@ -1,9 +1,15 @@
 package com.dice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude =
+{
+    ErrorMvcAutoConfiguration.class
+})
 public class DiceApplication
 {
     public static void main(String[] args)
